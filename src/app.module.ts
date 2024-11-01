8920b9bd2119f5437ca69f5844b528949cc4845f
ikabeee <carlglz30@gmail.com>
2024-10-31 19:44:33 -0500

    Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserController } from './user/user.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { MoService } from './auth/mo/mo.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [PrismaModule, UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [PrismaModule, UserModule, AuthModule],
+  controllers: [AppController, UserController, AuthController],
+  providers: [AppService, UserService, MoService, AuthService],
 })
 export class AppModule {}
