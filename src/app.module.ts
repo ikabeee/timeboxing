97544760e6bdb629ebcf193d11a61eb260d8761b
ikabeee <carlglz30@gmail.com>
2024-10-31 19:45:33 -0500

    Bug fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,12 @@ import { UserController } from './user/user.controller';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
-import { MoService } from './auth/mo/mo.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 
 @Module({
   imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController, UserController, AuthController],
-  providers: [AppService, UserService, MoService, AuthService],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule {}
