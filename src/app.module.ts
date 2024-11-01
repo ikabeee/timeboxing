014ad6f67e46d19bff4035e013a3e9c1f7fec6e4
ikabeee <carlglz30@gmail.com>
2024-10-31 21:20:30 -0500

    Automated changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,12 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, TaskModule],
   controllers: [AppController, UserController, AuthController],
-  providers: [AppService, UserService, AuthService],
+  providers: [AppService, UserService, AuthService, TaskService],
 })
 export class AppModule {}
